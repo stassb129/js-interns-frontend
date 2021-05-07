6854b9d31bea62271dd9ca05a0a8a1ec8584017e
stas <stassb129>
2021-05-07 12:03:31 +0300

    Test commit

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -15,7 +15,7 @@ const WelcomePage = () => {
 
                 <section className={css.topRated}>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cum dicta esse hic inventore itaque nemo
-                    perspiciatis ratione, suscipit. Aliquid aut cupiditate iste sint!
+                    perspiciatis ratione, suscipit. Aliquid aut cupiditate isteastas
                 </section>
 
             </div>
