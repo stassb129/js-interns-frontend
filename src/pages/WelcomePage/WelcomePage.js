2aa7f0caefd1f934330a722b486745812e307701
stassb129 <45415961+stassb129@users.noreply.github.com>
2021-05-10 19:37:33 +0300

    WelcomeParallaxEnd+ Top Rated

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -1,22 +1,53 @@
 import css from './welcomePage.module.scss';
-
 import WelcomeParallax from "../../components/WelcomeParallax/WelcomeParallax";
+import Header from "../../components/global/Header/Header";
+import SelectPanel from "../../components/SelectPanel/SelectPanel";
+import Button from "../../components/Button/Button";
+import hotel1 from '../../images/hotels/1.jpg'
+import hotel2 from '../../images/hotels/2.jpg'
+import hotel3 from '../../images/hotels/3.jpg'
+import hotel4 from '../../images/hotels/4.jpg'
+
 
 const WelcomePage = () => {
+
 
         return (
             <div className={css.welcome}>
 
-
                 <WelcomeParallax>
-                    header
+                    <Header/>
+                    <SelectPanel/>
+
+                    <div className={css.description}>
+                        <span>Discover rooms,</span>
+                        <span>Hotels & make assets!</span>
+                        <Button>Become a Seller</Button>
+                    </div>
                 </WelcomeParallax>
 
-
-                <section className={css.topRated}>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At cum dicta esse hic inventore itaque nemo
-                    perspiciatis ratione, suscipit. Aliquid aut cupiditate isteastas
+                <section className={css.topRatedSection}>
+                    <h3>Top Rated</h3>
+                    <div className={css.topRated}>
+                        <div className={css.topItem}>
+                            <div style={{backgroundImage: `url(${hotel1})`}}
+                                 className={css.itemPicture}></div>
+                        </div>
+                        <div className={css.topItem}>
+                            <div style={{backgroundImage: `url(${hotel2})`}}
+                                 className={css.itemPicture}></div>
+                        </div>
+                        <div className={css.topItem}>
+                            <div style={{backgroundImage: `url(${hotel3})`}}
+                                 className={css.itemPicture}></div>
+                        </div>
+                        <div className={css.topItem}>
+                            <div style={{backgroundImage: `url(${hotel4})`}}
+                                 className={css.itemPicture}></div>
+                        </div>
+                    </div>
                 </section>
+
 
             </div>
         );
