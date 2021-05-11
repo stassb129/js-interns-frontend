54a0bc96c035a71d0f6d43f075a2287a28fb5dcd
stas <stassb129>
2021-05-11 15:10:34 +0300

    Major Cities

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -8,6 +8,12 @@ import hotel2 from '../../images/hotels/2.jpeg'
 import hotel3 from '../../images/hotels/3.jpeg'
 import hotel4 from '../../images/hotels/4.jpeg'
 
+import los_angeles from '../../images/cities/los-angeles'
+import sun_francisco from '../../images/cities/sun-francisco'
+import miami from '../../images/cities/miami'
+import houston from '../../images/cities/houston.jpg'
+import new_york from '../../images/cities/new_york'
+
 
 const WelcomePage = () => {
         return (
@@ -24,7 +30,7 @@ const WelcomePage = () => {
                     </div>
                 </WelcomeParallax>
 
-                <section className={css.topRatedSection}>
+                <section>
                     <h3 className={css.sectionHeader}>Top Rated</h3>
                     <div className={css.topRated}>
                         <div className={css.topItem}>
@@ -43,6 +49,24 @@ const WelcomePage = () => {
                             <div style={{backgroundImage: `url(${hotel4})`}}
                                  className={css.itemPicture}></div>
                         </div>
+                    </div>
+                </section>
+
+                <section className={css.citiesSection}>
+                    <h3 className={css.sectionHeader}>
+                        Find Hotels in Major Cities
+                    </h3>
+                    <div className={css.gridMajorCities}>
+                        <div className={css.sun_francisco}
+                             style={{backgroundImage: `url(${sun_francisco})`}}></div>
+                        <div className={css.houston}
+                             style={{backgroundImage: `url(${houston})`}}></div>
+                        <div className={css.los_angeles}
+                             style={{backgroundImage: `url(${los_angeles})`}}></div>
+                        <div className={css.miami}
+                             style={{backgroundImage: `url(${miami})`}}></div>
+                        <div className={css.new_york}
+                             style={{backgroundImage: `url(${new_york})`}}></div>
                     </div>
                 </section>
 
